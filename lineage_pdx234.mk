e2de798db2c1bbd8ae7adb3d089dcd295c41cf7b
kyasu <kyasu.dev@gmail.com>
2026-07-12 10:13:22 +0900

    pdx234: Update blobs to XQ-DQ72_Customized_SEA_67.2.A.3.178
    Change-Id: Ic121a25b60b2c81ae1379aa95ed63948b2b0d0ba

--- lineage_pdx234.mk
+++ lineage_pdx234.mk
@@ -31,8 +31,8 @@ PRODUCT_BUILD_PROP_OVERRIDES := \
     DeviceProduct=XQ-DQ72 \
     SystemDevice=XQ-DQ72 \
     SystemName=XQ-DQ72 \
-    BuildFingerprint=Sony/XQ-DQ72/XQ-DQ72:15/67.2.A.3.163/067002A003016300521143226:user/release-keys \
-    BuildDesc="XQ-DQ72-user 15 67.2.A.3.163 067002A003016300521143226 release-keys"
+    BuildFingerprint=Sony/XQ-DQ72/XQ-DQ72:15/67.2.A.3.178/067002A003017800521143226:user/release-keys \
+    BuildDesc="XQ-DQ72-user 15 67.2.A.3.178 067002A003017800521143226 release-keys"
 
 PRODUCT_NAME := lineage_pdx234
 PRODUCT_DEVICE := pdx234
